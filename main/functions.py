7a325b65173cb1230072035048d1ab4e6b5134c6
eMotzArt <johnshuabratsk@gmail.com>
2022-06-19 22:59:11 +0700

    Поиск заработал

---
+++ main/functions.py
@@ -0,0 +1,13 @@
+import json
+
+def get_all_posts(file_name: str) -> list[dict]:
+    with open(file_name, encoding='utf-8') as data_base:
+        all_posts = json.load(data_base)
+    return all_posts
+
+def search_post_by_str(search_str: str, posts: list[dict]) -> list[dict]:
+    posts_to_return = []
+    for post in posts:
+        if search_str in post['content']:
+            posts_to_return.append(post)
+    return posts_to_return
--- main/views.py
+++ main/views.py
@@ -1,6 +1,17 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
+from main.functions import get_all_posts, search_post_by_str
 
-main_blueprint = Blueprint('main_blueprint', __name__, template_folder='./templates')
+main_blueprint = Blueprint('main_blueprint', __name__, template_folder='./templates', static_folder='/static')
+
 @main_blueprint.route('/')
 def main_page():
-    return render_template('main.html')
+    return render_template('index.html')
+
+@main_blueprint.route('/search')
+def search_page():
+    search_line = request.args['s']
+    all_posts = get_all_posts('posts.json')
+    sorted_posts = search_post_by_str(search_line, all_posts)
+    return render_template('searched_posts.html', search_line=search_line, posts=sorted_posts)
+    pass
+    return "Я ищу"
